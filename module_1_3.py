19b7ce80e36cf6c3ea02db9680f6bd17708de251
Ruslan-16 <ruslanalmasovich@gmail.com>
2024-09-16 15:02:03 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+from array import typecodes
+
+name='Ruslan'
+print(name)
+age=37
+print(age)
+new_age= 1
+print(age+new_age)
+name='is_student'
+print(name,type(name))
---
+++ урок строки и индексация строк.py
@@ -0,0 +1,10 @@
+name = 'Ruslan'
+print("hello "+name )
+name = "Ruslan"
+print(name[0] )
+name = "Ruslan"
+print(name[:2] )
+name = "Ruslan"
+print(name[0:6:2] )
+name = "Ruslan"
+print(name[::-1] )
