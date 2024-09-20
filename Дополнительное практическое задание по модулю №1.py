ec239b4dd2156f6623145a851feb2ea324a71d58
Ruslan <ruslanalmasovich@gmail.com>
2024-09-20 09:58:32 +0300

    Add files via upload

--- Дополнительное практическое задание по модулю №1.py
+++ Дополнительное практическое задание по модулю №1.py
@@ -1,9 +1,8 @@
 # Задание "Средний балл":
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-journal= {'Johnny': sum([4,5,5,2])/len([4,5,5,2]),'Bilbo':sum([2,2,2,3])/len([2,2,2,3]),
-          'Steve': sum([5,5,5,4,5])/len([5,5,5,4,5]), 'Khendrik':sum([4,4,3])/len([4,4,3]),
-          'Aaron':sum([5,3,3,5,4])/len([5,3,3,5,4])}
+grades =([[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]])
+print(grades)
+students=sorted({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
+print(students)
+journal={'Aaron':sum([5,3,3,5,4])/len([5,3,3,5,4]),'Bilbo':sum([2,2,2,3])/len([2,2,2,3]),'Johnny': sum([4,5,5,2])/len([4,5,5,2]),
+           'Khendrik':sum([4,4,3])/len([4,4,3]),'Steve': sum([5,5,5,4,5])/len([5,5,5,4,5])}
 print(journal)
-
-
