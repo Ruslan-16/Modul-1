0aa543f17d6df46382247dff2eb302f9b34638d9
Ruslan <ruslanalmasovich@gmail.com>
2024-09-20 11:10:10 +0300

    Add files via upload

--- Дополнительное практическое задание по модулю №1.py
+++ Дополнительное практическое задание по модулю №1.py
@@ -5,9 +5,14 @@ grades =([sum([5, 3, 3, 5, 4])/len([5, 3, 3, 5, 4]), sum([2, 2, 2, 3])/len([2, 2
           sum([4, 5, 5, 2])/len([4, 5, 5, 2]), sum([5, 5, 5, 4, 5])/len([5, 5, 5, 4, 5])])
 students=sorted({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
 print(students)
-journal={'Aaron':grades[0],'Bilbo':grades[1],'Johnny':grades[2] ,
-          'Khendrik':grades[3],'Steve':grades[4]}
+journal=dict(zip(students, grades))
 print(journal)
+#journal={students[0]:grades[0],students[1]:grades[1],students[2]:grades[2] ,
+          students[3]:grades[3],students[4]:grades[4]}
+#print(journal)
+
+
+
 
 #grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 #students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
@@ -16,5 +21,5 @@ print(journal)
 #         'Aaron':sum(grades[0])/len(grades[0])}
 #print(journal)
 
-#journal=dict(zip(students, grades))
+
 #print(journal)
